53101e0fff83ad203f668ca4d86ed628dba60200
Jonathan Brunet <jonathan.brunet@progilone.fr>
2018-02-01 01:00:50 +0100

    Table gym

--- migrations/20180201001035_create_gym_table.js
+++ migrations/20180201001035_create_gym_table.js
@@ -1,9 +1,15 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('user', function (t) {
+  return knex.schema.createTable('gym', function (t) {
     t.increments('id').primary()
-    t.string('username').notNullable()
-    t.string('password').notNullable()
+	t.string('gym_id').notNullable()
+	t.timestamp('last_modified')
+	t.timestamp('last_scanned')
+    t.string('name').notNullable()
+    t.string('latitude').notNullable()
+	t.string('longitude').notNullable()
+	t.boolean('sponsor')
+	t.string('team_id')
+	t.integer('slots_available')
     t.timestamps(false, true)
   }) 
 };
